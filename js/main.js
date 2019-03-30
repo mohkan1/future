258e67eff4f29ab4e5b39956b95acdd865610994
mohkan <maher.99875@gmail.com>
2019-03-30 12:08:20 +0100

    best

--- js/main.js
+++ js/main.js
@@ -70,11 +70,6 @@ document.querySelector('form').addEventListener('submit', function (event) {
    var commitTitle = document.getElementById('commit-title').value;
    var fileName = document.getElementById('fileName').value;
 
-
-   for (var i = 0; i < 5; i++) {
-     uploadFiles(fileName, editor.getValue(), commitTitle);
-
-   }
    uploadFiles(fileName, editor.getValue(), commitTitle)
       .then(function() {
          alert('Your file has been saved correctly.');
